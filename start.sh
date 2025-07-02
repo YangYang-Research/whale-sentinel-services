9ac7eb1edab4e445b5705757fee940e8fbd1a11a
noobpk <ltp.noobpk@gmail.com>
2025-07-02 21:58:46 +0700

    Release: 0.1.3 - Fix: Ensure correct permissions for log directory in start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,6 @@ mkdir -p /var/log/whale-sentinel/ws-services/ws-dga-detection
 mkdir -p /var/log/whale-sentinel/ws-services/ws-common-attack-detection
 mkdir -p /var/log/whale-sentinel/ws-services/ws-web-attack-detection
 
+chmod 750 /var/log/whale-sentinel/
+
 docker-compose up -d
